c9a6bcb5cebb0fd53e7ed2f9113cbb36a4baf350
GaebTheDev <gaeb.mislang.dev@gmail.com>
2024-03-29 18:07:37 +0800

    Added custom animations and responsive login

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,27 @@ module.exports = {
       fontFamily: {
         quattrocento: ["Quattrocento", "serif"],
         "quattrocento-sans": ["Quattrocento-sans", "sans-serif"],
+      },
+      keyframes: {
+        wiggle: {
+          '25%': { transform: 'rotate(-2deg)' },
+          '50%': { transform: 'rotate(2deg)' },
+          '75%': { transform: 'rotate(-2deg)' },
+        },
+        float: {
+          '33%': { transform: 'translateY(6px)' },
+          '66%': { transform: 'translateY(-6px)' },
+        },
+        zoom: {
+          '100%' : {transform: 'scale(1.25)'}
+        },
+      },
+      animation:{
+        wiggle: 'wiggle 2s ease-in-out infinite',
+        wiggleOnce: 'wiggle 1s ease-in-out',
+        float: 'float 2s linear infinite',
+        floatOnce: 'float 2s ease-in-out',
+        zoom: 'zoom 1s ease-in-out forwards',
       }
     },
   },
